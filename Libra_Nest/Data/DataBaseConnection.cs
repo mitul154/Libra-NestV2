059ae0e643a1fa44d9156c02d1eb451d5061e950
mitul72 <mituldhawan2@gmail.com>
2023-08-18 21:30:33 -0600

    added database

--- Libra_Nest/Data/DataBaseConnection.cs
+++ Libra_Nest/Data/DataBaseConnection.cs
@@ -15,12 +15,12 @@ namespace Libra_Nest.Data
         private static string ConnectionString = $"Data Source={dir};Version=3;";
 
 
-        public static List<Books> LoadBooks()
+        public static List<Books> LoadBooks(string query = "select * from Books")
         {
 
             using (IDbConnection cnn = new SQLiteConnection(ConnectionString))
             {
-                var output = cnn.Query<Books>("select * from Books", new DynamicParameters());
+                var output = cnn.Query<Books>(query, new DynamicParameters());
                 return output.ToList();
             }
         }
@@ -31,7 +31,15 @@ namespace Libra_Nest.Data
                 cnn.Execute($"insert into Books (Title, Author) values ('{book.title}', '{book.author}')");
             }
         }
-        
+        public static void Update(Books book)
+        {
+            using (IDbConnection cnn = new SQLiteConnection(ConnectionString))
+            {
+                cnn.Execute($"Update Books set Copies='${book.copies}' where Title='{book.title}'");
+            }
+        }
+
+
 
     }
 }
